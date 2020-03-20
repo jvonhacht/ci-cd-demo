4a7a1fc5cb783193232f3007798b56907aa8817a
Johan von Hacht <johvh@kth.se>
2020-03-20 16:05:56 +0100

    fix ssh key on server

--- web/app.py
+++ web/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
  
 @app.route('/')
 def hello_world():
-    return '<h1> Hello there! <h1><p>hello</p><br><p>korvar</p> <font color="red">CORONANANdsadANA!</font> '
+    return '<h1> Hello there! <h1><p>hello</p><br><p>korvar</p> <font color="red">COdsadRONANANdsadANA!</font> '
  
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
